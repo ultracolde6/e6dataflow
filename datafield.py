0276a92f2ee9e5b0f5616d99ab76009d93a3a6e9
jagerber48 <justin.gerber48@gmail.com>
2021-01-08 23:50:28 -0800

    add DataStreamShotDataField

--- datafield.py
+++ datafield.py
@@ -2,16 +2,39 @@ from datamodel import InputParamLogger
 
 
 class DataField(InputParamLogger):
-    def __init__(self, *, datafield_name):
+    def __init__(self, *, datafield_name, datamodel):
         self.datafield_name = datafield_name
+        self.datamodel = datamodel
 
 
 class ShotDataField(DataField):
-    def __init__(self, *, datafield_name):
-        super(ShotDataField, self).__init__(datafield_name=datafield_name)
+    def __init__(self, *, datafield_name, datamodel):
+        super(ShotDataField, self).__init__(datafield_name=datafield_name, datamodel=datamodel)
 
     def get_data(self, shot_num):
         raise NotImplementedError
 
     def set_data(self, shot_num):
         raise NotImplementedError
+
+
+class DataStreamShotDataField(ShotDataField):
+    def __init__(self, *, datafield_name, datamodel, datastream_name, h5_subpath, h5_dataset_name):
+        super(DataStreamShotDataField, self).__init__(datafield_name=datafield_name, datamodel=datamodel)
+        self.datastream_name = datastream_name
+        self.h5_subpath = h5_subpath
+        self.h5_dataset_name = h5_dataset_name
+        self.datastream = datamodel.datastream_dict[datastream_name]
+
+    def get_data(self, shot_num):
+        h5_file = self.datastream.load_shot(shot_num)
+        if self.h5_subpath is not None:
+            h5_group = h5_file[self.h5_subpath]
+        else:
+            h5_group = h5_file
+        data = h5_group[self.h5_dataset_name]
+        return data
+
+    def set_data(self, shot_num):
+        raise UserWarning('Cannot set data in datastream datafields. These datafields represent raw data which should'
+                          'not be manipulated')
