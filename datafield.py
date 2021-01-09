3078f680bf694dd9e3c1907bb95a8e61c060535d
jagerber48 <justin.gerber48@gmail.com>
2021-01-08 23:26:21 -0800

    More work on processor, starting to create DataField classes

--- datafield.py
+++ datafield.py
@@ -2,5 +2,16 @@ from datamodel import InputParamLogger
 
 
 class DataField(InputParamLogger):
-    def __init__(self):
-        pass
+    def __init__(self, *, datafield_name):
+        self.datafield_name = datafield_name
+
+
+class ShotDataField(DataField):
+    def __init__(self, *, datafield_name):
+        super(ShotDataField, self).__init__(datafield_name=datafield_name)
+
+    def get_data(self, shot_num):
+        raise NotImplementedError
+
+    def set_data(self, shot_num):
+        raise NotImplementedError
--- processor.py
+++ processor.py
@@ -4,27 +4,29 @@ from utils import to_list, shot_to_loop_and_point
 
 
 class Processor(InputParamLogger):
-    def __init__(self, *, processor_name):
+    def __init__(self, *, processor_name, datamodel):
         self.processor_name = processor_name
+        self.datamodel = datamodel
 
-    def process(self, datamodel, shot_num):
+    def process(self, shot_num):
         raise NotImplementedError
 
 
 class CountsProcessor(Processor):
-    def __init__(self, *, processor_name, frame_datafield_name, result_datafield_name, roi_slice_list):
-        super(CountsProcessor, self).__init__(processor_name=processor_name)
+    def __init__(self, *, processor_name, datamodel, frame_datafield_name, result_datafield_name, roi_slice_list):
+        super(CountsProcessor, self).__init__(processor_name=processor_name, datamodel=datamodel)
         self.frame_datafield_name = frame_datafield_name
         self.result_datafield_name = result_datafield_name
         self.roi_slice_list = to_list(roi_slice_list)
+        self.datamodel
 
-    def process(self, datamodel, shot_num):
-        frame = datamodel.get_shot_data(self.frame_datafield_name, shot_num)
+    def process(self, shot_num):
+        frame = self.datamodel.get_shot_data(self.frame_datafield_name, shot_num)
         if len(self.roi_slice_list) > 1:
-            if len(self.roi_slice_list) != datamodel.num_points:
+            if len(self.roi_slice_list) != self.datamodel.num_points:
                 raise ValueError(f'Length of roi_slice ({len(self.roi_slice_list):d}) is not equal to datamodel'
-                                 f'num_points ({datamodel.num_points})')
-            loop_num, point_num = shot_to_loop_and_point(shot_num, datamodel.num_points)
+                                 f'num_points ({self.datamodel.num_points})')
+            loop_num, point_num = shot_to_loop_and_point(shot_num, self.datamodel.num_points)
             roi_slice = self.roi_slice_list[point_num]
             roi_frame = frame[roi_slice]
             counts = np.nansum(roi_frame)
@@ -32,4 +34,4 @@ class CountsProcessor(Processor):
             roi_slice = self.roi_slice_list[0]
             roi_frame = frame[roi_slice]
             counts = np.nansum(roi_frame)
-        datamodel.set_shot_data(self.result_datafield_name, shot_num, counts)
+        self.datamodel.set_shot_data(self.result_datafield_name, shot_num, counts)
