b6f029cd6d96ee65ffc4048e339e85ed78166641
Justin Gerber <justin.gerber48@gmail.com>
2021-04-03 07:12:54 -0600

    add full ROI function, improve fit failure detection and some code cleanup/organization.

--- tools/tweezer_tools.py
+++ tools/tweezer_tools.py
@@ -10,12 +10,12 @@ from e6dataflow.utils import make_centered_roi, get_shot_list_from_point
 
 def interpolate_tweezer_positions(first_tweezer_vert, first_tweezer_horiz,
                                   last_tweezer_vert, last_tweezer_horiz,
-                                  num_tweezers):
-    tweezer_vert_spacing = (last_tweezer_vert - first_tweezer_vert) / (num_tweezers - 1)
-    tweezer_horiz_spacing = (last_tweezer_horiz - first_tweezer_horiz) / (num_tweezers - 1)
+                                  num_tweezer):
+    tweezer_vert_spacing = (last_tweezer_vert - first_tweezer_vert) / (num_tweezer - 1)
+    tweezer_horiz_spacing = (last_tweezer_horiz - first_tweezer_horiz) / (num_tweezer - 1)
     vert_center_list = []
     horiz_center_list = []
-    for tweezer_num in range(num_tweezers):
+    for tweezer_num in range(num_tweezer):
         vert_center = first_tweezer_vert + tweezer_num * tweezer_vert_spacing
         horiz_center = first_tweezer_horiz + tweezer_num * tweezer_horiz_spacing
         vert_center_list.append(vert_center)
@@ -24,57 +24,77 @@ def interpolate_tweezer_positions(first_tweezer_vert, first_tweezer_horiz,
     return vert_center_list, horiz_center_list
 
 
-def fit_for_roi(img, vert_center, horiz_center, vert_span, horiz_span, lock_span=True, span_scale=3.0):
+def fit_for_roi(img, vert_center_guess, horiz_center_guess, vert_search_span, horiz_search_span,
+                lock_span=True, span_output_factor=3.0):
 
-    horiz_halfspan = np.ceil(horiz_span / 2)
-    lower_horiz = int(horiz_center - horiz_halfspan)
-    upper_horiz = int(horiz_center + horiz_halfspan)
+    horiz_halfspan = np.ceil(horiz_search_span / 2)
+    lower_horiz = int(horiz_center_guess - horiz_halfspan)
+    upper_horiz = int(horiz_center_guess + horiz_halfspan)
 
-    vert_halfspan = np.ceil(vert_span / 2)
-    lower_vert = int(vert_center - vert_halfspan)
-    upper_vert = int(vert_center + vert_halfspan)
+    vert_halfspan = np.ceil(vert_search_span / 2)
+    lower_vert = int(vert_center_guess - vert_halfspan)
+    upper_vert = int(vert_center_guess + vert_halfspan)
 
     fit_img = img[lower_vert:upper_vert, lower_horiz:upper_horiz]
     fit_dict = fit_gaussian2d(fit_img, show_plot=False, lightweight=True)
 
-    horiz_span_result = int(horiz_span)
-    vert_span_result = int(vert_span)
-    success = fit_dict['success']
-    if success:
-        horiz0_result = fit_dict['x0']['val'] + lower_horiz
-        vert0_result = fit_dict['y0']['val'] + lower_vert
-        if not lock_span:
-            horiz_span_result = int(span_scale * fit_dict['sx']['val'])
-            vert_span_result = int(span_scale * fit_dict['sy']['val'])
-    else:
-        horiz0_result = horiz_center
-        vert0_result = vert_center
+    horiz_center_fit = fit_dict['x0']['val'] + lower_horiz
+    vert_center_fit = fit_dict['y0']['val'] + lower_vert
+    horiz_sigma_fit = fit_dict['sx']['val']
+    vert_sigma_fit = fit_dict['sy']['val']
 
-    vert_slice, horiz_slice = make_centered_roi(vert0_result, horiz0_result, vert_span_result, horiz_span_result)
+    success = fit_dict['success']
+    if not lower_horiz < horiz_center_fit < upper_horiz:
+        success = False
+    if not lower_vert < vert_center_fit < upper_vert:
+        success = False
+    if not horiz_search_span / 10 < horiz_sigma_fit < horiz_search_span:
+        success = False
+    if not vert_search_span / 10 < vert_sigma_fit < vert_search_span:
+        success = False
+
+    if success:
+        horiz_center_output = horiz_center_fit
+        vert_center_output = vert_center_fit
+        if not lock_span:
+            horiz_span_output = int(span_output_factor * horiz_sigma_fit)
+            vert_span_output = int(span_output_factor * vert_sigma_fit)
+        else:
+            horiz_span_output = horiz_search_span
+            vert_span_output = vert_search_span
+    else:
+        horiz_center_output = horiz_center_guess
+        vert_center_output = vert_center_guess
+        horiz_span_output = horiz_search_span
+        vert_span_output = vert_search_span
+
+    vert_slice, horiz_slice = make_centered_roi(vert_center_output, horiz_center_output,
+                                                vert_span_output, horiz_span_output)
     return vert_slice, horiz_slice, success
 
 
-def generate_pzt_point_frame_dict(num_pzt, num_point, num_frame, mode='single',
+def generate_pzt_point_frame_dict(num_pzt, num_points, frame_list, mode='single',
                                   num_inner_point_loop=None, num_outer_point_loop=None):
     pzt_point_frame_dict = dict()
+    num_frames = len(frame_list)
 
     if mode == 'single':
         if num_pzt != 1:
             raise ValueError('num_pzt must equal 1 for mode=\'single\'.')
         point_frame_list = []
-        for point_num in range(num_point):
-            for frame_num in range(num_frame):
+        for point_num in range(num_points):
+            for frame_num in frame_list:
                 point_frame_tuple = (point_num, frame_num)
                 point_frame_list.append(point_frame_tuple)
         pzt_point_frame_dict[0] = point_frame_list
 
     if mode == 'frames':
-        if num_pzt != num_frame:
-            raise ValueError('num_pzt must equal num_frame for mode=\'frames\'.')
+        if num_pzt != num_frames:
+            raise ValueError('num_pzt must equal num_frames for mode=\'frames\'.')
         for pzt_num in range(num_pzt):
             point_frame_list = []
             frame_num = pzt_num
-            for point_num in range(num_point):
+            for point_num in range(num_points):
                 point_frame_tuple = (point_num, frame_num)
                 point_frame_list.append(point_frame_tuple)
             pzt_point_frame_dict[pzt_num] = point_frame_list
@@ -88,7 +108,7 @@ def generate_pzt_point_frame_dict(num_pzt, num_point, num_frame, mode='single',
             outer_point_num = pzt_num
             for inner_point_num in range(num_inner_point_loop):
                 point_num = outer_point_num * num_inner_point_loop + inner_point_num
-                for frame_num in range(num_frame):
+                for frame_num in frame_list:
                     point_frame_tuple = (point_num, frame_num)
                     point_frame_list.append(point_frame_tuple)
             pzt_point_frame_dict[pzt_num] = point_frame_list
@@ -102,18 +122,18 @@ def generate_pzt_point_frame_dict(num_pzt, num_point, num_frame, mode='single',
             inner_point_num = pzt_num
             for outer_point_num in range(num_outer_point_loop):
                 point_num = outer_point_num * num_inner_point_loop + inner_point_num
-                for frame_num in range(num_frame):
+                for frame_num in frame_list:
                     point_frame_tuple = (point_num, frame_num)
                     point_frame_list.append(point_frame_tuple)
             pzt_point_frame_dict[pzt_num] = point_frame_list
 
     if mode == 'points':
-        if num_pzt != num_point:
-            raise ValueError('num_pzt must equal num_point for mode=\'points\'.')
+        if num_pzt != num_points:
+            raise ValueError('num_pzt must equal num_points for mode=\'points\'.')
         for pzt_num in range(num_pzt):
             point_frame_list = []
             point_num = pzt_num
-            for frame_num in range(num_frame):
+            for frame_num in frame_list:
                 point_frame_tuple = (point_num, frame_num)
                 point_frame_list.append(point_frame_tuple)
             pzt_point_frame_dict[pzt_num] = point_frame_list
@@ -135,20 +155,20 @@ def get_frame_from_h5(h5_path, frame_num):
 
 def get_avg_frame_over_loops(data_dir, data_prefix, point_num, frame_num, num_points, max_shot_num=None):
     num_shots = get_num_shots(data_dir)
-    shot_list, num_loops = get_shot_list_from_point(point=point_num, num_points=num_points, num_shots=num_shots)
+    if max_shot_num is None:
+        max_shot_num = num_shots
+    shot_list, num_loops = get_shot_list_from_point(point=point_num, num_points=num_points, num_shots=max_shot_num)
     first_shot_h5_path = Path(data_dir, f'{data_prefix}_00000.h5')
     avg_frame = np.zeros_like(get_frame_from_h5(first_shot_h5_path, frame_num=frame_num))
     for loop_num, shot_num in enumerate(shot_list):
-        if shot_num <= max_shot_num:
-            h5_path = Path(data_dir, f'{data_prefix}_{shot_num:05d}.h5')
-            avg_frame += get_frame_from_h5(h5_path, frame_num=frame_num) / num_loops
-            print(f'point: {point_num}, frame: {frame_num}, loop: {loop_num}, shot: {shot_num}')
+        h5_path = Path(data_dir, f'{data_prefix}_{shot_num:05d}.h5')
+        avg_frame += get_frame_from_h5(h5_path, frame_num=frame_num) / num_loops
     return avg_frame
 
 
 def get_roi_dict(data_dir, data_prefix, num_points, pzt_point_frame_dict,
                  vert_center_list, horiz_center_list,
-                 vert_span, horiz_span, lock_span=True, span_scale=3.0, max_shot_num=None):
+                 vert_search_span, horiz_search_span, lock_span=True, span_output_factor=3.0, max_shot_num=None):
     first_shot_h5_path = Path(data_dir, f'{data_prefix}_00000.h5')
     blank_frame = np.zeros_like(get_frame_from_h5(first_shot_h5_path, frame_num=0))
     num_tweezer = len(vert_center_list)
@@ -172,9 +192,10 @@ def get_roi_dict(data_dir, data_prefix, num_points, pzt_point_frame_dict,
             horiz_center = horiz_center_list[tweezer_num]
             vert_slice, horiz_slice, success = fit_for_roi(tot_avg_frame,
                                                            vert_center, horiz_center,
-                                                           vert_span=vert_span, horiz_span=horiz_span,
+                                                           vert_search_span=vert_search_span,
+                                                           horiz_search_span=horiz_search_span,
                                                            lock_span=lock_span,
-                                                           span_scale=span_scale)
+                                                           span_output_factor=span_output_factor)
             roi_tuple_list.append((vert_slice, horiz_slice))
             roi_tuple_string = f'({vert_slice.start}, {vert_slice.stop}) x ({horiz_slice.start}, {horiz_slice.stop})'
             if not success:
@@ -182,13 +203,42 @@ def get_roi_dict(data_dir, data_prefix, num_points, pzt_point_frame_dict,
             roi_tuple_string_list.append(roi_tuple_string)
             plot_vert_span = vert_slice.stop - vert_slice.start
             plot_horiz_span = horiz_slice.stop - horiz_slice.start
-            rect = patches.Rectangle((horiz_slice.start, vert_slice.start),
-                                     plot_horiz_span, plot_vert_span,
-                                     linewidth=1, edgecolor='w', facecolor='none')
-            ax.add_patch(rect)
+            rect_guess = patches.Rectangle((horiz_center - horiz_search_span / 2, vert_center - vert_search_span / 2),
+                                           horiz_search_span, vert_search_span,
+                                           linewidth=1, edgecolor='r', facecolor='none')
+            rect_fit = patches.Rectangle((horiz_slice.start, vert_slice.start),
+                                         plot_horiz_span, plot_vert_span,
+                                         linewidth=1, edgecolor='w', facecolor='none')
+            ax.add_patch(rect_guess)
+            ax.add_patch(rect_fit)
         roi_string = ('ROI List\n (vert_start, vert_stop) x (horiz_start, horiz_stop)\n'
                       + '\n'.join([roi_tuple_string for roi_tuple_string in roi_tuple_string_list]))
         ax.text(x=2, y=0.5, s=roi_string, transform=ax.transAxes, verticalalignment='center')
         output_pzt_roi_dict[pzt_key] = roi_tuple_list
 
     return output_pzt_roi_dict
+
+
+def auto_roi(data_dir, data_prefix,
+             num_tweezers, num_points, frame_list, num_pzt, mode,
+             first_tweezer_vert, first_tweezer_horiz,
+             last_tweezer_vert, last_tweezer_horiz,
+             vert_search_span, horiz_search_span, lock_span):
+    vert_center_list, horiz_center_list = interpolate_tweezer_positions(first_tweezer_vert,
+                                                                        first_tweezer_horiz,
+                                                                        last_tweezer_vert,
+                                                                        last_tweezer_horiz,
+                                                                        num_tweezers)
+
+    pzt_point_frame_dict = generate_pzt_point_frame_dict(num_pzt=num_pzt, num_points=num_points,
+                                                         frame_list=frame_list, mode=mode)
+
+    pzt_roi_dict = get_roi_dict(data_dir=data_dir, data_prefix=data_prefix,
+                                num_points=num_points,
+                                pzt_point_frame_dict=pzt_point_frame_dict,
+                                vert_center_list=vert_center_list,
+                                horiz_center_list=horiz_center_list,
+                                vert_search_span=vert_search_span,
+                                horiz_search_span=horiz_search_span,
+                                lock_span=lock_span)
+    return pzt_roi_dict, pzt_point_frame_dict
